4ee5b1ed63eaa9a90e53276eabe52c3cedb502a9
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2025-01-02 17:49:59 +0100

    refactor test code

--- test/test_circle_detection.py
+++ test/test_circle_detection.py
@@ -147,32 +147,32 @@ class TestCircleDetection:
         )
         bandwidth = 0.07
 
-        circle_detection = CircleDetection(bandwidth=bandwidth)
-        circle_detection.detect(xy, num_workers=-1)
-        circle_detection.filter(max_circles=1, num_workers=-1)
+        circle_detector = CircleDetection(bandwidth=bandwidth)
+        circle_detector.detect(xy, num_workers=-1)
+        circle_detector.filter(max_circles=1, num_workers=-1)
 
-        assert len(circle_detection.circles) == 1
-        assert len(circle_detection.fitting_losses) == 1
-        np.testing.assert_array_equal(circle_detection.batch_lengths_circles, np.array([1], dtype=np.int64))
+        assert len(circle_detector.circles) == 1
+        assert len(circle_detector.fitting_losses) == 1
+        np.testing.assert_array_equal(circle_detector.batch_lengths_circles, np.array([1], dtype=np.int64))
 
         # the first circle is expected to be returned because its points have lower variance
-        np.testing.assert_almost_equal(original_circles[0], circle_detection.circles[0], decimal=10)
+        np.testing.assert_almost_equal(original_circles[0], circle_detector.circles[0], decimal=10)
 
-        circle_detection.detect(xy, num_workers=-1)
-        circle_detection.filter(max_circles=2, non_maximum_suppression=True, num_workers=-1)
+        circle_detector.detect(xy, num_workers=-1)
+        circle_detector.filter(max_circles=2, non_maximum_suppression=True, num_workers=-1)
 
-        assert len(circle_detection.circles) == 2
-        assert len(circle_detection.fitting_losses) == 2
-        np.testing.assert_array_equal(circle_detection.batch_lengths_circles, np.array([2], dtype=np.int64))
+        assert len(circle_detector.circles) == 2
+        assert len(circle_detector.fitting_losses) == 2
+        np.testing.assert_array_equal(circle_detector.batch_lengths_circles, np.array([2], dtype=np.int64))
 
         expected_fitting_losses = []
-        for circle in circle_detection.circles:
+        for circle in circle_detector.circles:
             residuals = (np.linalg.norm(xy - circle[:2], axis=-1) - circle[2]) / bandwidth
             expected_loss = -1 / np.sqrt(2 * np.pi) * np.exp(-1 / 2 * residuals**2)
             expected_fitting_losses.append(expected_loss.sum())
 
-        assert (np.abs(original_circles - circle_detection.circles) < 0.01).all()
-        np.testing.assert_almost_equal(expected_fitting_losses, circle_detection.fitting_losses, decimal=5)
+        assert (np.abs(original_circles - circle_detector.circles) < 0.01).all()
+        np.testing.assert_almost_equal(expected_fitting_losses, circle_detector.fitting_losses, decimal=5)
 
     def test_several_noisy_circles(self):
         original_circles = self._generate_circles(
@@ -187,8 +187,8 @@ class TestCircleDetection:
         min_start_xy = xy.min(axis=0) - 2
         max_start_xy = xy.max(axis=0) + 2
 
-        circle_detection = CircleDetection(bandwidth=0.05, min_fitting_score=5)
-        circle_detection.detect(
+        circle_detector = CircleDetection(bandwidth=0.05, min_fitting_score=5)
+        circle_detector.detect(
             xy,
             min_start_x=min_start_xy[0],
             max_start_x=max_start_xy[0],
@@ -207,17 +207,17 @@ class TestCircleDetection:
             break_max_radius=1.5,
             num_workers=-1,
         )
-        circle_detection.filter(deduplication_precision=3, non_maximum_suppression=True)
+        circle_detector.filter(deduplication_precision=3, non_maximum_suppression=True)
 
-        assert len(original_circles) == len(circle_detection.circles)
-        assert len(circle_detection.circles) == len(circle_detection.fitting_losses)
+        assert len(original_circles) == len(circle_detector.circles)
+        assert len(circle_detector.circles) == len(circle_detector.fitting_losses)
         np.testing.assert_array_equal(
-            circle_detection.batch_lengths_circles, np.array([len(original_circles)], dtype=np.int64)
+            circle_detector.batch_lengths_circles, np.array([len(original_circles)], dtype=np.int64)
         )
 
         for original_circle in original_circles:
             matches_with_detected_circle = False
-            for detected_circle in circle_detection.circles:
+            for detected_circle in circle_detector.circles:
                 if (np.abs(original_circle - detected_circle) < 0.03).all():
                     matches_with_detected_circle = True
                     break
@@ -234,12 +234,12 @@ class TestCircleDetection:
         if pass_max_dist:
             max_dist = bandwidth
 
-        circle_detection = CircleDetection(bandwidth=bandwidth)
-        circle_detection.detect(
+        circle_detector = CircleDetection(bandwidth=bandwidth)
+        circle_detector.detect(
             xy,
             num_workers=-1,
         )
-        circle_detection.filter(
+        circle_detector.filter(
             max_circles=1,
             min_circumferential_completeness_idx=0.9,
             circumferential_completeness_idx_max_dist=max_dist,
@@ -247,16 +247,16 @@ class TestCircleDetection:
             num_workers=-1,
         )
 
-        assert len(circle_detection.circles) == 1
-        assert len(circle_detection.fitting_losses) == 1
+        assert len(circle_detector.circles) == 1
+        assert len(circle_detector.fitting_losses) == 1
 
-        np.testing.assert_almost_equal(original_circles[0], circle_detection.circles[0], decimal=10)
+        np.testing.assert_almost_equal(original_circles[0], circle_detector.circles[0], decimal=10)
 
-        circle_detection.detect(
+        circle_detector.detect(
             xy[:50],
             num_workers=-1,
         )
-        circle_detection.filter(
+        circle_detector.filter(
             max_circles=1,
             min_circumferential_completeness_idx=0.9,
             circumferential_completeness_idx_max_dist=max_dist,
@@ -264,8 +264,8 @@ class TestCircleDetection:
             num_workers=-1,
         )
 
-        assert len(circle_detection.circles) == 0
-        assert len(circle_detection.fitting_losses) == 0
+        assert len(circle_detector.circles) == 0
+        assert len(circle_detector.fitting_losses) == 0
 
     def test_batch_processing(self):
         original_circles = np.array([[0, 0, 0.5], [0, 0, 0.52]])
@@ -273,26 +273,26 @@ class TestCircleDetection:
         xy_2 = self._generate_circle_points(original_circles[1:], min_points=100, max_points=100, variance=0.0)
         batch_lengths = np.array([len(xy_1), len(xy_2)], dtype=np.int64)
 
-        circle_detection = CircleDetection(bandwidth=0.05)
+        circle_detector = CircleDetection(bandwidth=0.05)
 
-        circle_detection.detect(
+        circle_detector.detect(
             np.concatenate((xy_1, xy_2)),
             batch_lengths=batch_lengths,
             num_workers=-1,
         )
-        circle_detection.filter(max_circles=1, num_workers=-1)
+        circle_detector.filter(max_circles=1, num_workers=-1)
 
         num_batches = len(batch_lengths)
-        assert len(circle_detection.circles) == num_batches
-        assert len(circle_detection.fitting_losses) == num_batches
+        assert len(circle_detector.circles) == num_batches
+        assert len(circle_detector.fitting_losses) == num_batches
 
-        batch_starts = np.cumsum(np.concatenate((np.array([0]), circle_detection.batch_lengths_circles)))[:-1]
+        batch_starts = np.cumsum(np.concatenate((np.array([0]), circle_detector.batch_lengths_circles)))[:-1]
 
         for batch_idx in range(num_batches):
             batch_start = batch_starts[batch_idx]
-            batch_end = batch_start + circle_detection.batch_lengths_circles[batch_idx]
+            batch_end = batch_start + circle_detector.batch_lengths_circles[batch_idx]
             np.testing.assert_almost_equal(
-                original_circles[batch_idx].reshape(-1, 3), circle_detection.circles[batch_start:batch_end], decimal=5
+                original_circles[batch_idx].reshape(-1, 3), circle_detector.circles[batch_start:batch_end], decimal=5
             )
 
     @pytest.mark.skipif(multiprocessing.cpu_count() <= 1, reason="Testing of multi-threading requires multiple cores.")
@@ -302,7 +302,7 @@ class TestCircleDetection:
         xy_2 = self._generate_circle_points(original_circles[1:], min_points=100, max_points=100, variance=0.0)
         batch_lengths = np.array([len(xy_1), len(xy_2)], dtype=np.int64)
 
-        circle_detection = CircleDetection(bandwidth=0.05)
+        circle_detector = CircleDetection(bandwidth=0.05)
 
         single_threaded_runtime = 0
         multi_threaded_runtime = 0
@@ -310,14 +310,14 @@ class TestCircleDetection:
         repetitions = 4
         for _ in range(repetitions):
             start = time.time()
-            circle_detection.detect(
+            circle_detector.detect(
                 np.concatenate((xy_1, xy_2)),
                 batch_lengths=batch_lengths,
                 num_workers=1,
             )
             single_threaded_runtime += time.time() - start
             start = time.time()
-            circle_detection.detect(
+            circle_detector.detect(
                 np.concatenate((xy_1, xy_2)),
                 batch_lengths=batch_lengths,
                 num_workers=-1,
@@ -328,12 +328,12 @@ class TestCircleDetection:
 
     def test_empty_input(self):
         xy = np.empty((0, 2), dtype=np.float64)
-        circle_detection = CircleDetection(bandwidth=0.05)
-        circle_detection.detect(xy)
+        circle_detector = CircleDetection(bandwidth=0.05)
+        circle_detector.detect(xy)
 
-        assert len(circle_detection.circles) == 0
-        assert len(circle_detection.fitting_losses) == 0
-        assert circle_detection.batch_lengths_circles.sum() == 0
+        assert len(circle_detector.circles) == 0
+        assert len(circle_detector.fitting_losses) == 0
+        assert circle_detector.batch_lengths_circles.sum() == 0
 
     @pytest.mark.parametrize(
         "kwargs",
@@ -397,10 +397,10 @@ class TestCircleDetection:
         for arg_name, arg_value in kwargs.items():
             args[arg_name] = arg_value
 
-        circle_detection = CircleDetection(bandwidth=0.01)
+        circle_detector = CircleDetection(bandwidth=0.01)
 
         with pytest.raises(ValueError):
-            circle_detection.detect(
+            circle_detector.detect(
                 xy,
                 **args,
             )
@@ -419,13 +419,13 @@ class TestCircleDetection:
         for arg_name, arg_value in kwargs.items():
             args[arg_name] = arg_value
 
-        circle_detection = CircleDetection(bandwidth=0.01)
-        circle_detection.detect(xy)
+        circle_detector = CircleDetection(bandwidth=0.01)
+        circle_detector.detect(xy)
 
         with pytest.raises(ValueError):
-            circle_detection.filter(**args)
+            circle_detector.filter(**args)
 
     def test_filtering_without_detection(self):
-        circle_detection = CircleDetection(bandwidth=0.01)
+        circle_detector = CircleDetection(bandwidth=0.01)
         with pytest.raises(ValueError):
-            circle_detection.filter()
+            circle_detector.filter()
