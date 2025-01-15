096b5f50f45235b7dd2b540b2579be4148bf6cd7
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2025-01-15 11:01:34 +0100

    format C++ code

--- csrc/pybind/circle_detection_pybind.cpp
+++ csrc/pybind/circle_detection_pybind.cpp
@@ -11,32 +11,37 @@ PYBIND11_MODULE(_circle_detection_cpp, m) {
     Circle detection in 2D point sets.
   )pbdoc";
 
-  m.def("detect_circles_m_estimator", &CircleDetection::detect_circles_m_estimator<float>,
-        pybind11::return_value_policy::reference_internal, "");
+  m.def(
+      "detect_circles_m_estimator", &CircleDetection::detect_circles_m_estimator<float>,
+      pybind11::return_value_policy::reference_internal, "");
 
-  m.def("detect_circles_m_estimator", &CircleDetection::detect_circles_m_estimator<double>,
-        pybind11::return_value_policy::reference_internal,
-        R"pbdoc(
+  m.def(
+      "detect_circles_m_estimator", &CircleDetection::detect_circles_m_estimator<double>,
+      pybind11::return_value_policy::reference_internal,
+      R"pbdoc(
     C++ implementation of the M-estimator-based circle detection method proposed by Tim Garlipp and Christine H.
     Müller. For more details, see the documentation of the Python wrapper class
     :code:`circle_detection.MEstimator`.
   )pbdoc");
 
-  m.def("detect_circles_ransac", &CircleDetection::detect_circles_ransac<float>,
-        pybind11::return_value_policy::reference_internal, "");
+  m.def(
+      "detect_circles_ransac", &CircleDetection::detect_circles_ransac<float>,
+      pybind11::return_value_policy::reference_internal, "");
 
-  m.def("detect_circles_ransac", &CircleDetection::detect_circles_ransac<double>,
-        pybind11::return_value_policy::reference_internal,
-        R"pbdoc(
+  m.def(
+      "detect_circles_ransac", &CircleDetection::detect_circles_ransac<double>,
+      pybind11::return_value_policy::reference_internal,
+      R"pbdoc(
     C++ implementation of RANSAC circle detection that is based on least-squares circle fitting. For more details, see
     the documentation of the Python wrapper class :code:`circle_detection.Ransac`.
   )pbdoc");
 
-  m.def("fit_circle_lsq", &CircleDetection::fit_circle_lsq<float>, pybind11::return_value_policy::reference_internal,
-        "");
+  m.def(
+      "fit_circle_lsq", &CircleDetection::fit_circle_lsq<float>, pybind11::return_value_policy::reference_internal, "");
 
-  m.def("fit_circle_lsq", &CircleDetection::fit_circle_lsq<double>, pybind11::return_value_policy::reference_internal,
-        R"pbdoc(
+  m.def(
+      "fit_circle_lsq", &CircleDetection::fit_circle_lsq<double>, pybind11::return_value_policy::reference_internal,
+      R"pbdoc(
     C++ implementation of least-squares circle fitting. For more details, see the documentation of the Python wrapper
     method :code:`circle_detection.fit_circle_lsq()`.
   )pbdoc");
