6c7befd73c39164ebd64bf374062b2d1510ebdf8
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2025-12-29 22:51:33 +0100

    fix pipeline

--- csrc/include/CircleDetection/operations.h
+++ csrc/include/CircleDetection/operations.h
@@ -158,6 +158,8 @@ ArrayX<scalar_T> circumferential_completeness_index(
             return std::atan2(y, x);
           });
 
+      std::cout << "angles " << angles << std::endl;
+
       // ArrayXl sections =
       //     (angles / angular_step_size).floor().unaryExpr([](scalar_T x) { return static_cast<int64_t>(x); });
       // sections = sections.unaryExpr([num_regions_copy](const int64_t x) { return x % num_regions_copy; });
