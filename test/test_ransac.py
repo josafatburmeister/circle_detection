fb815f68848a8e9b9cd3cedef7564ad5f2466f71
Josafat-Mattias Burmeister <burmeister@uni-potsdam.de>
2025-12-31 13:58:58 +0100

    add debug logs

--- test/test_ransac.py
+++ test/test_ransac.py
@@ -62,6 +62,7 @@ class TestRansac:
 
     @pytest.mark.skipif(multiprocessing.cpu_count() <= 1, reason="Testing of multi-threading requires multiple cores.")
     def test_multi_threading(self):
+        print("multiprocessing.cpu_count()", multiprocessing.cpu_count())
         batch_size = 80
 
         xy = []
